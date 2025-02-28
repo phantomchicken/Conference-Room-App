f4aa5ed3fbe8754e0cf2ec4b2d4b620a532c4746
Nino Brezac <nino.brezac@gmail.com>
2025-02-28 14:00:08 +0100

    Bootstrap, material table

--- Conference-Room-App/src/app/app.component.ts
+++ Conference-Room-App/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TemplateComponent } from './template/template.component';
+import {MatTableModule} from '@angular/material/table';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [TemplateComponent, RouterOutlet],
+  imports: [TemplateComponent, RouterOutlet, MatTableModule],
   template: `<router-outlet></router-outlet>`,
   styleUrls: ['./app.component.css']
 })
